e954aa1abee0d2f9f89f940e083197161336ec1f
asojka09 <austinsojka@hotmail.com>
2016-01-24 13:56:59 -0600

    changed namespace

--- init.cs
+++ init.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace testform
+namespace metagame
 {
 	public class MainClass
 	{
--- main.cs
+++ main.cs
@@ -5,7 +5,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
-namespace testform
+namespace metagame
 {
 	public class MainForm : Form
 	{
